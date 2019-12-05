183e47acc29616c327d9717dd0cb94e08bf5e5e3
harumo11 <s_harumo@hotmail.co.jp>
2019-12-05 18:59:10 +0900

    [add] added src

---
+++ src/pose_follow.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+int main(int argc, char* argv[])
+{
+	ros::init(argc, argv, "pose_follow_node");
+	
+	return 0;
+}
