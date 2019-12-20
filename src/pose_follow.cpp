76f01df5cf42ccb87f7e242ac5ab3a99abc4408b
harumo11 <s_harumo@hotmail.co.jp>
2019-12-21 01:48:42 +0900

    [update] for now

--- src/pose_follow.cpp
+++ src/pose_follow.cpp
@@ -1,9 +1,17 @@
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 int main(int argc, char* argv[])
 {
 	ros::init(argc, argv, "pose_follow_node");
-	
+	ros::NodeHandle node_handler;
+
+	// コントローラのスイッチが押されたら{local_controller_frame},{local_hand_frame}を作成する．
+	// {local_controller_frame} : viveコントローラの初期位置の座標
+	// {local_hand_frame} : 
+
+
 	return 0;
 }
